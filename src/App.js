f71037c5375d78e52a40f5798897cec09c2ae36b
anjalikesavarapu <anjalikesavarapu@gmail.com>
2022-05-16 10:32:40 +0530

    1 binaries uploaded

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import Info from './components/Info.js';
+import './style.css';
 
 export default function App() {
   return (
     <div>
+      <Info />
       <h1>Hello StackBlitz!</h1>
       <p>Start editing to see some magic happen :)</p>
     </div>
---
+++ src/components/Info.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function Info() {
+  return (
+    <div>
+      <img src="../images/myimage.jpg" className="my-image" />
+      <h3 className="my-name">Anjali Kesavarapu</h3>
+      <h4 className="my-role">Fullstack Developer</h4>
+      <h5 className="my-website">My website</h5>
+      <button className="btn-email">Email</button>
+      <button className="btn-linkedin">LinkedIn</button>
+    </div>
+  );
+}
